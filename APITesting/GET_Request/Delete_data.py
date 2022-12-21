c64f0fade4924f3f4b3cffd25414d6199fafb439
GeethaNetha <118677392+GeethaNetha@users.noreply.github.com>
2022-12-21 19:37:38 +0530

    Add files via upload

---
+++ APITesting/GET_Request/Delete_data.py
@@ -0,0 +1,13 @@
+import requests
+import jsonpath
+ValidUrl="http://trainingapi.jalaacademy.com/api/users/247"
+response=requests.delete(ValidUrl)
+#fetch response code
+print(response)
+assert response.status_code==500
+
+#invalid url
+InvalidUrl='http://trainingapi.jalaacademy.com/api/users/49'
+Url_response=requests.delete(InvalidUrl)
+print(Url_response)
+
